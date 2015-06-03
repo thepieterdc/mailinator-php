c277369d77282f827e75bbb2b9bbd1fdf45252f5
Skylar Sadlier <skylord123@gmail.com>
2015-06-03 00:20:53 -0600

    Update Mailinator.php
    Brackets should be broken out for better reading - Best practice.

--- Mailinator.php
+++ Mailinator.php
@@ -1,14 +1,17 @@
 <?php
-class Mailinator {
+class Mailinator
+{
 	private $token;
 	private $apiEndpoint = "https://api.mailinator.com/api/";
 	private $inboxCount = 0;
 	
-	public function __construct($token) {
+	public function __construct($token)
+	{
 		$this->token = $token;
 	}
 	
-	private function call($method,$params) {
+	private function call($method, $params)
+	{
 		$params['token'] = $this->token;
 		$params_str = $this->paramsToString($params);
 		$url = $this->apiEndpoint.$method."?".$params_str;
@@ -18,30 +21,37 @@ class Mailinator {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$exec = curl_exec($ch);
 		$info = curl_getinfo($ch);
-		if($info["http_code"] == 200) {
+		if($info["http_code"] == 200)
+		{
 			curl_close($ch);
 			return json_decode($exec,true);
-		} else {
+		}
+		else
+		{
 			die("An error happened");
 		}
 	}
 	
-	private function paramsToString($params = array()) {
+	private function paramsToString($params = array())
+	{
 		$str = "";
-		foreach($params as $key=>$val) {
+		foreach($params as $key=>$val)
+		{
 			$str .= $key."=".$val."&";
 		}
 		return rtrim($str, "&");
 	}
 	
-	public function fetchInbox($inbox) {
+	public function fetchInbox($inbox)
+	{
 		$query = $this->call('inbox',array('to' => $inbox));
 		$mailbox = $query["messages"];
 		$this->inboxCount = count($mailbox);
 		return $mailbox;
 	}
 	
-	public function fetchMail($msgId) {
+	public function fetchMail($msgId)
+	{
 		$query = $this->call('email',array('id' => $msgId));
 		$message = $query["data"];
 		var_dump($message);
